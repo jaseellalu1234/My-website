7e0488f00d98ac82562ed43d1781191f18bec1d9
jaseellalu1234 <jaseel.vpmpd@gmail.com>
2024-08-29 00:49:18 +0530

    NavbarBase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import Navbar from "./Components/Navbar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import "../Components/Styles/Navbar.css";
+import { FaBars } from "react-icons/fa6";
+function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <>
+      <header>
+        <div className="container">
+          <nav>
+            <div className="logo">
+              <h1>JASEELLALU</h1>
+            </div>
+            <ul className={isOpen ? "nav-link active" : "nav-link"}>
+              <li>
+                <a href="#">Works</a>
+              </li>
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+            </ul>
+            <div className="icon" onClick={toggleMenu}>
+              <FaBars />
+            </div>
+          </nav>
+        </div>
+      </header>
+      <section></section>
+    </>
+  );
+}
+
+export default Navbar;
